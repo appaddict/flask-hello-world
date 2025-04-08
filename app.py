7a8eeefe9afe09dbc6ecae75d21afc1221504ef2
appaddict <139108287+appaddict@users.noreply.github.com>
2025-04-08 21:56:58 +0800

    Update app.py v2

--- app.py
+++ app.py
@@ -3,4 +3,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+
+    my_html =  '''
+    
+    <body style="font-size:10vw">
+    
+    
+        <h1 style="color:  #002aff;">Hello World</h1>
+    
+    </body>
+    
+    ''' 
+    return my_html
